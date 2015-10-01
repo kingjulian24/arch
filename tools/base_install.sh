1f4078b0fe183997bb2b34a0e7d811116c9fb0a4
Nelson Ripoll <nripoll@peer1.com>
2015-10-01 01:19:18 -0500

    updated base install script

--- tools/base_install.sh
+++ tools/base_install.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
-pacman -S --noconfirm git zsh vim-python3 python-pip xorg-server xorg-xdm xorg-xinit qiv abs dmenu rxvt-unicode yajl
+
+# variables
+POWERLINE_FONTS="https://github.com/powerline/fonts.git"
+POWERLINE_SYMBOLS="https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf"
+POWERLINE_CONF="https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf"
+
+read -r CMNDS <<EOF
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -i --noconfirm
+
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -i --noconfirm
+EOF
+
+read -r PKGS <<EOF
+git zsh vim-python3 python-pip 
+xorg-server xorg-xdm xorg-xinit 
+qiv abs dmenu rxvt-unicode yajl
+EOF
+
+read -r ARCH <<EOF
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=/dev/sda2 rw
+EOF
+
+read -r BOOT <<EOF
+default arch
+timeout 3
+EOF
+
+
+# packages
+pacman -S --noconfirm $PKGS 
 
 
 # locale
@@ -21,28 +57,35 @@ hwclock --systohc --utc
 # boot loader
 bootctl install
 
-cat > /boot/loader/entries/arch.conf <<EOF
-title Arch Linux
-linux /vmlinuz-linux
-initrd /initramfs-linux.img
-options root=/dev/sda2 rw
-EOF
-
-cat > /boot/loader/loader.conf <<EOF
-default arch
-timeout 3
-EOF
+echo "$ARCH" > /boot/loader/entries/arch.conf 
+echo "$BOOT" > /boot/loader/loader.conf
 
 
 # powerline and patched fonts
 pip install powerline-status
 
-git clone --depth=1 https://github.com/powerline/fonts.git /tmp/fonts
+curl -L $POWERLINE_SYMBOLS -o /usr/share/fonts/PowerlineSymbols.otf
+curl -L $POWERLINE_CONF -o /etc/fonts/conf.d/11-powerline-symbols.conf
 
+git clone --depth=1 $POWERLINE_FONTS /tmp/fonts
 mv /tmp/fonts/* /usr/share/fonts/
 
-curl -L https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -o /usr/share/fonts/PowerlineSymbols.otf
-
-curl -L https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -o /etc/fonts/conf.d/11-powerline-symbols.conf
-
 fc-cache -vf /usr/share/fonts
+
+
+# enable network
+systemctl enable dhcpcd@enp0s3.service
+
+
+# create user for non-root tasks
+cp /etc/sudoers /etc/sudoers.bkp
+
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+useradd -m -G wheel -s /bin/bash arch-user
+
+su arch-user -c "$CMNDS"
+
+userdel arch-user
+
+mv /etc/sudoers.bkp /etc/sudoers
