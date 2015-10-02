32692c7d9e02a192d770b16ef7eea30068e302aa
Nelson Ripoll <nripoll@peer1.com>
2015-10-02 01:11:44 -0500

    added mount commands

--- tools/base_install.sh
+++ tools/base_install.sh
@@ -9,6 +9,10 @@ sgdisk -n 2:1050623:$ENDSECTOR  -c 2:"ARCH"  -t 2:8300 /dev/sda
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4      /dev/sda2
 
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
 pacstrap /mnt base base-devel
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
