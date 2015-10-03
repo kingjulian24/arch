f145548c125e7051f3e6ecb110e2914e7b2bb762
Nelson Ripoll <nripoll@peer1.com>
2015-10-02 22:15:48 -0500

    made script more readable

--- tools/base_install.sh
+++ tools/base_install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+
+# clear hard disk
 sgdisk -og /dev/sda
 
-ENDSECTOR=`sgdisk -E /dev/sda`
+# boot partition
+START_SECTOR=$(sgdisk -F /dev/sda)
 
-sgdisk -n 1:2048:1050623        -c 1:"EFI"   -t 1:EF00 /dev/sda
-sgdisk -n 2:1050623:$ENDSECTOR  -c 2:"ARCH"  -t 2:8300 /dev/sda
+sgdisk  -n 1:$STARTSECTOR:+512M  -c 1:"EFI"  -t 1:EF00 /dev/sda
 
+
+# root partition
+START_SECTOR=$(sgdisk -F /dev/sda)
+  END_SECTOR=$(sgdisk -E /dev/sda)
+
+sgdisk  -n 2:1050623:$ENDSECTOR  -c 2:"ARCH"  -t 2:8300 /dev/sda
+
+
+# format partitions
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4      /dev/sda2
 
+# mount partitions
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
+# install linux
 pacstrap /mnt base base-devel
 
+# generate fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
