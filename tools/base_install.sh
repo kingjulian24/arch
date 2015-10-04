8b25f2c8a1eaf55b497565ddd450f4f8cc882d04
Nelson Ripoll <nripoll@peer1.com>
2015-10-03 23:24:22 -0500

    automated post install

--- tools/base_install.sh
+++ tools/base_install.sh
@@ -20,13 +20,20 @@ sgdisk  -n 2:$FIRST_SECTOR:$END_SECTOR  -c 2:"ARCH"  -t 2:8300 /dev/sda
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4      /dev/sda2
 
+
 # mount partitions
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
+
 # install linux
 pacstrap /mnt base base-devel
 
+
 # generate fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
+
+# post install
+arch-chroot /mnt /bin/bash -c '"sh -c $(https://raw.github.com/nelsonripoll/arch/master/tools/post_install.sh)"'
