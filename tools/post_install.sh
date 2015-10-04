365fc9d77b27a1d34f717ff18635e724840d2690
Nelson Ripoll <nripoll@peer1.com>
2015-10-03 23:24:06 -0500

    made yaourt install script

--- tools/post_install.sh
+++ tools/post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pacman -S --noconfirm git yajl
 
 # boot loader
 read -r -d '' ARCH <<'EOF'
@@ -45,38 +44,5 @@ echo "localhost" > /etc/hostname
 systemctl enable dhcpcd@enp0s3.service
 
 
-# yaourt
-cd /tmp
-
-read -r -d '' SUDOERS <<'EOF'
-root ALL=(ALL) ALL
-%wheel ALL=(ALL) NOPASSWD: ALL
-EOF
-
-read -r -d '' CMNDS <<'EOF'
-git clone https://aur.archlinux.org/package-query.git /tmp/package-query
-cd /tmp/package-query
-makepkg -i --noconfirm
-
-git clone https://aur.archlinux.org/yaourt.git /tmp/yaourt
-cd /tmp/yaourt
-makepkg -i --noconfirm
-EOF
-
-echo $SUDOERS >> /etc/sudoers
-
-cp /etc/sudoers /etc/sudoers.backup
-
-useradd -m -G wheel -s /bin/bash arch-user
-
-su arch-user -c "$CMNDS"
-
-userdel arch-user
-
-mv /etc/sudoers.backup /etc/sudoers
-
-
 # initramfs
 mkinitcpio -p linux
-
-cd ~
